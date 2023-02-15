b3f95e127ba8e817f36a7f38fae8e69f191d1821
Rodolfo Schmidt <rodolfoschmidt9@gmail.com>
2023-02-15 15:18:26 -0300

    add hamburgerMenu and JS

--- main.js
+++ main.js
@@ -2,7 +2,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuHamburger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 navEmail.addEventListener('click', toggleNavEmail);
+menuHamburger.addEventListener('click', toggleMobileMenu);
+
 function toggleNavEmail() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
