7336008eef821a5d6b870eaab1dae25c63042260
Rodolfo Schmidt <rodolfoschmidt9@gmail.com>
2023-02-17 10:12:50 -0300

    Agregando funcionalidad al carrito y nav, tanto css como js

--- main.js
+++ main.js
@@ -5,13 +5,43 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const cartnav = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 navEmail.addEventListener('click', toggleNavEmail);
 menuHamburger.addEventListener('click', toggleMobileMenu);
+cartnav.addEventListener('click', toggleCartAside);
 
 function toggleNavEmail() {
+    const isCartAsideClosed = aside.classList.contains('inactive');
+    //si cart abierto lo cerramos.
+    if (!isCartAsideClosed) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isCartAsideClosed = aside.classList.contains('inactive');
+    //si cart abierto lo cerramos.
+    if (!isCartAsideClosed) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+
+}
+
+function toggleCartAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isNavEmailClosed = navEmail.classList.contains('inactive');
+
+    //si mobile esta abierto lo cerramos.
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+
+    } if (!isNavEmailClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
