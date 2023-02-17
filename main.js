2f6bbbd9b2aab98166b58a025b5573d83b3a842b
Rodolfo Schmidt <rodolfoschmidt9@gmail.com>
2023-02-17 13:09:05 -0300

    Agregando las cards y sus funciones js

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const cartnav = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector('.cards-container');
+
 navEmail.addEventListener('click', toggleNavEmail);
 menuHamburger.addEventListener('click', toggleMobileMenu);
 cartnav.addEventListener('click', toggleCartAside);
@@ -45,3 +47,65 @@ function toggleCartAside() {
 
     aside.classList.toggle('inactive');
 }
+
+/*Array productos*/
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pantalla',
+    price: 12000,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Computadora',
+    price: 20000,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+//Creamos una función para tomar un array, y renderizar las cards en este caso
+
+function renderProductList(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.img);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+
+        const productFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productFigure.appendChild(productImgCard);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+//invocamos la funcion con el parametro que es el array con la lista de productos.
+renderProductList(productList);
